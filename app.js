9f2b717ed9e0545373078fe99eb93a938bda693f
ThanhLe62 <137408389+ThanhLe62@users.noreply.github.com>
2023-07-20 02:49:50 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+// bai tap1
+// let Number = [1, 2, 3];
+// console.log("Input:",Number);
+// let sumNumber = 1 + 2 + 3;
+// console.log("Output: "sumNumber);
+
+// bai tap2
+// let number = [1, 2, 3];
+// console.log("Input :",number);
+// let maxNumber =  number[0];
+// for (let i = 1; i < number.length; i++) {
+//     if (number[i] > maxNumber) {
+//         maxNumber = number[i];
+//     }
+//     }
+//             console.log("Output :",maxNumber);
+
+// Bai tap3
+// let number = [10, 8, 2, 6];
+// console.log("Input :",number);
+// let minNumber =  number[0];
+// for (let i = 1; i < number.length; i++) {
+//     if (number[i] < minNumber) {
+//         minNumber = number[i];
+//     }
+//     }
+//             console.log("Output :",minNumber);
+
+// Bai tap4
+// Cach1
+// let a = [1, 2, 3, 4, 5];
+// let b = [6];
+// if(b = a){console.log("true")}
+// else{console.log("false");}
+// Cach 2
+// let number = [1, 2, 3, 4, 5];
+// let searchNumber = number.includes(30);
+// console.log(searchNumber);
+
+// Bai tap 5
+// let a = [1, 2, 3, 4, 5];
+// console.log("Input:",a);
+// let reverseArray = a.reverse();
+// console.log("Output:",reverseArray);
+
+// Bai tap 6
+// let x = [1, 2, 3, 4, 5];
+// result = x.filter((a) => {
+//     return a % 2 == 0;
+// })
+
+// Bai tap 8
+let numbers = [4, 2, 9, 5, 1];
+console.log(numbers);
+let sortArr = numbers.sort();
+console.log(sortArr);
+
+
+   
+
+
+
+
+        
+        
